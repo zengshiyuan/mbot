46ad8fad828ac2592494ef9e57ceb84b28804691
zengshiyuan <3268463239@qq.com>
2023-09-03 22:15:31 +0800

    add qr_track.cpp

--- pose_nav/src/start.cpp
+++ pose_nav/src/start.cpp
@@ -7,7 +7,6 @@ int main(int argc, char **argv)
     ros::init(argc,argv,"start");
     ros::Duration(5);
     system("roslaunch mbot_gazebo view_mbot_with_laser_gazebo.launch");
-    // execlp("roslaunch" "roslaunch", "mbot_gazebo","view_mbot_with_laser_gazebo.launch",NULL );
-    // execl("roslaunch" "roslaunch", "mbot_gazebo","view_mbot_with_laser_gazebo.launch",NULL );
+ 
     return 0;
 }
